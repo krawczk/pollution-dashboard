acaebfb13ef84bf9c6d3df7ef2ad6ae9499d68e3
krawczk <kacperkrawczyk490@gmail.com>
2023-04-23 13:46:34 +0200

    Setup a temporary Username and Password for ArcGIS Api

--- src/config.py
+++ src/config.py
@@ -5,3 +5,7 @@ FEATURE_SERVICE_URL = os.environ.get("FEATURE_SERVICE_URL")
 CLIENT_ID = os.environ.get("CLIENT_ID")
 CLIENT_SECRET = os.environ.get("CLIENT_SECRET")
 MEASURES = ["PM10", "PM2.5", "CO", "SO2", "NO2"]
+
+# TODO TEMPORARY
+USERNAME = os.environ.get("USERNAME")
+PASSWORD = os.environ.get("PASSWORD")
