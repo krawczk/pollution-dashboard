0a71e4e6fbf3674f7122742f812ab8ae5ee8e78a
krawczk <kacperkrawczyk490@gmail.com>
2023-04-26 17:25:55 +0200

    Refactor get_gios_pollution_data to handle long data type

--- src/gios_steps_definitions.py
+++ src/gios_steps_definitions.py
@@ -8,14 +8,14 @@ import requests
 from config import MEASURES
 
 
-def get_gios_pollution_data():
-    pollution_df = pd.DataFrame()
-    station_data = _fetch_gios_station_data()
-    daily_data = []
+def get_gios_pollution_data(is_long=False):
+    pollution_data = []
+    all_station_data = _fetch_gios_all_station_data()
     today_date = datetime.now().strftime("%Y-%m-%d %H")
 
-    for station_id in station_data["id"]:
-        station_df = pd.DataFrame()
+    for station_id in all_station_data["id"]:
+        station_data = all_station_data[all_station_data["id"] == station_id]
+        station_pollution_df = pd.DataFrame()
         try:
             sensors_id = _fetch_gios_sensor_data(station_id)["id"]
         except KeyError:
@@ -25,37 +25,36 @@ def get_gios_pollution_data():
             if not isinstance(sensor_data, pd.DataFrame):
                 logging.warning(f"No available sensor data for given {station_id}")
             sensor_data.rename(columns={"key": "measure", "value": "pollution_value"}, inplace=True)
-            station_df = pd.concat([station_df, sensor_data])
+            station_pollution_df = pd.concat([station_pollution_df, sensor_data])
 
         # Remove any nan values
-        station_df.dropna(inplace=True)
+        station_pollution_df.dropna(inplace=True)
 
         # Filter the date after obtaining all the data from sensor_ids for given station
-        station_df = station_df[station_df["date"].str.contains(today_date)].sort_values(by=["date"])
-        station_df["date"] = pd.to_datetime(station_df["date"]).dt.strftime("%Y-%m-%dT%H:%M:%SZ")
+        station_pollution_df = station_pollution_df[station_pollution_df["date"].str.contains(today_date)].sort_values(by=["date"])
+        station_pollution_df["date"] = pd.to_datetime(station_pollution_df["date"]).dt.strftime("%Y-%m-%dT%H:%M:%SZ")
 
         # Add station id and name to dataframe
-        station_df["name"] = station_data[station_data["id"] == station_id]["stationName"].iloc[0]
+        station_pollution_df["name"] = station_data["stationName"].iloc[0]
 
         # Add the location data to the dataframe
-        lat = station_data[station_data["id"] == station_id]["gegrLat"].iloc[0]
-        lon = station_data[station_data["id"] == station_id]["gegrLon"].iloc[0]
-
-        for filter_date in station_df["date"].unique():
-            pollution_data = {"Name": station_data[station_data["id"] == station_id]["stationName"].iloc[0],
-                              "Date": filter_date, "LAT": lat, "LON": lon}
-            df_filtered_by_date = station_df[station_df["date"] == filter_date]
-            for measure in df_filtered_by_date["measure"]:
-                if measure in MEASURES:
-                    df_filtered_by_measure = df_filtered_by_date[df_filtered_by_date["measure"] == measure]
-                    pollution_data[measure] = df_filtered_by_measure["pollution_value"].values[0]
-            daily_data.append(pollution_data)
-
-    pollution_df = pollution_df.from_dict(daily_data)
+        lat = station_data["gegrLat"].iloc[0]
+        lon = station_data["gegrLon"].iloc[0]
+        for measure in station_pollution_df["measure"]:
+            if measure in MEASURES:
+                station_pollution_data_dict = {"Name": station_data[station_data["id"] == station_id]["stationName"].iloc[0],
+                                  "Date": today_date, "LAT": lat, "LON": lon}
+                if is_long:
+                    station_pollution_data_dict["MEASURE"] = measure
+                else:
+                    df_filtered_by_measure = station_pollution_df[station_pollution_df["measure"] == measure]
+                    station_pollution_data_dict[measure] = df_filtered_by_measure["pollution_value"].values[0]
+                pollution_data.append(station_pollution_data_dict)
+    pollution_df = pd.DataFrame.from_dict(pollution_data)
     return pollution_df
 
 
-def _fetch_gios_station_data() -> Union[pd.DataFrame, str]:
+def _fetch_gios_all_station_data() -> Union[pd.DataFrame, str]:
     r = requests.get("https://api.gios.gov.pl/pjp-api/rest/station/findAll")
     if r.status_code == 200:
         return pd.DataFrame(r.json())
